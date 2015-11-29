fe39aa88b9813e7a3e92ce820e0965a359601731
Devin Jiang <devin.jiang@temple.edu>
2015-11-29 00:42:53 -0500

    Update - one word histogram

--- hw_08/foo.py
+++ hw_08/foo.py
@@ -5,17 +5,23 @@
 # import the following modules
 import os
 import sys
+import operator
+import time
 from foo_00 import WordCount
 
 # main
 def main(argv):
+    # start timer
+    tic = time.clock()
+
     # check command line arguments
     if len(argv) == 1:
         sys.exit("Missing argument(s)")
     # end if
 
+    # initialize and parse command line arguments
     filename = argv[1]
-    word = argv[2]
+    search_term = argv[2]
 
     try:
         # open file
@@ -30,13 +36,47 @@ def main(argv):
     # create object
     database_obj = WordCount()
 
+    # initialize count for the number of file
     num_file = 0
-    #
+    num_words = 0
+    tmp = []
+    record = {}
+    # loop through every line/path in the txt file
     for path in f:
+        # remove the new line characters
         path = path.rstrip("\n")
-        db_list = database_obj.read(path)
-        num_file += database_obj.containsWord(db_list, word)
-    print "A total of %d files contained the word '%s'" %(num_file, word)
+
+        # call the readwc method to read the txt files
+        db_list = database_obj.readwc(path)
+
+        # count the number files with the search term entered via command line arg
+        num_file += database_obj.containsWord(db_list, search_term)[0]
+        list_with_word = database_obj.containsWord(db_list, search_term)[1]
+
+        if len(list_with_word) != 0:
+            for word in list_with_word:
+                if not record.has_key(word):
+                    record[word] = 1
+                else:
+                    record[word] = record[word] + 1
+        
+    # end for loop
+
+    num_words = database_obj.totalWords(record)
+    avg_wpf = float(num_words)/float(num_file)
+    database_obj.createHistogram(record,search_term)
+    #
+    # end timer
+    toc = time.clock()
+
+    # calculate the runtime
+    runtime = float(toc - tic)
+
+    # display results
+    print "A total of %d files contained the word '%s'" %(num_file, search_term)
+    print "A total of %d words occurred in %d files: the average words per file is %.2f." %(num_words, num_file, avg_wpf)
+    print "Total runtime: %f s" %(runtime)
+    # close file(s)
     f.close()
 # end main
 
--- hw_08/foo_00.py
+++ hw_08/foo_00.py
@@ -5,21 +5,32 @@
 import os
 import sys
 import re
+import operator
+from collections import Counter
 
-# class
 class WordCount():
 
-    # method: read
+    # method: readwc
+    # arguments: filepath - path to each txt file
+    # return: a list of every word in the txt file
     #
-    def read(self, filepath):
+    def readwc(self, filepath):
+        # declare a list
+        txt_ls = []
+
         # check if the file exists
-        words_ls = []
         try:
             # open file
             with open(filepath, 'r') as content_file:
+                # read the entire file and store content
                 content = content_file.read()
-                content = re.sub("[^A-Za-z0-9']+", ' ', content)
-                words_ls = content.rstrip('\r\n').lower().rsplit()
+
+                # remove special characters
+                content = re.sub("[^A-Za-z0-9]+", ' ', content)
+
+                # remove new line, make lower case, split word by word into list
+                txt_ls = content.rstrip('\r\n').lower().rsplit()
+            # end open
         # end try
 
         # if not then exit the program
@@ -28,20 +39,61 @@ class WordCount():
             sys.exit()
         # end except
 
-        return words_ls
+        return txt_ls
+    # end method readwc
 
-    # end method read
-
-    # method
-    def containsWord(self, words_ls, word):
+    # method: containsWord
+    # arguments: list words_ls, string words
+    # return: count = 1 if word is in the list or 0 if it is not
+    #
+    def containsWord(self, txt_ls, search_term):
+        # initialize and declare count
         count = 0
-        if any(word in index for index in words_ls):
+        word_ls = []
+        # search for any occurrances of the word in the list
+        # include an variation of the word
+        # ex: 'spikes', if word is 'spike'
+        if any(search_term in index for index in txt_ls):
+            # set count to 1
             count = 1
-        return count
-    # end method search
+
+            #
+            word_ls = txt_ls
+
+        return (count, word_ls)
+
+
+
+    # end method containsWord
+
+
+    def totalWords(self, record):
+        total = 0
+        for word in record:
+            total += int(record[word])
+        return total
+
+    # method:
+    def createHistogram(self, record, search_term):
+        total = 0
+
+        print "-"*(61 + len(search_term))
+        print "Histogram of all the word in all files that have the word '%s'." %(search_term)
+        print "-"*(61 + len(search_term))
+        total = self.totalWords(record)
+        for word in sorted(record, key=record.get, reverse=True):
+            # word = record[i][0]
+            # count = record[i][1]
+            width = 36 - len(word)
+            percentage = float(record[word])/total * 100
+
+            print "%s %s %.6f %%" %(str(word), str(" "*width), percentage )
+    # end createHistogram
 
     # method
-    def createHistogram():
-        pass
-    # end method createHistogram
+    def twoWordList(self,txt_ls):
+        two_words = []
+        for i in range(0,len(txt_ls) - 1):
+            two_words.append(txt_ls[i] + " " + txt_ls[i+1])
+        return two_words
 # end class WordCount
